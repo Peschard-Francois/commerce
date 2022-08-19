9af7f6531bc52513c79cfab292c0241a03826cfa
Peschard-Francois <francois.glenard@gmail.com>
2022-08-19 03:01:38 +0200

    Association table

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -11,9 +14,37 @@ class HomeController extends AbstractController {
      * @Route("/", name="homepage")
      */
 
-    public function homepage(){
+    public function homepage(ProductRepository $productRepository,EntityManagerInterface $em){
+/*       dump($productRepository->count(['price' => 1500])) ;*/
+/*        $product = $productRepository->find(2);*/
+        /*$product = $productRepository->findAll();*/
+        /*$product = $productRepository->findBy([
+            'slug' => 'chaise-en-bois',
+            'price'=> 2000
+        ],['name' => 'DESC']
+        );*/
+   /*     $product = $productRepository->findOneBy([],['name' => 'DESC'];*/
+       /* dump($product->getUppercaseName());*/
+
+  /*      $productRepository = $em->getRepository(Product::class);
+        $product =$productRepository->find(3);
+       /* $product->setPrice(2500);*/ //modifier
+
+        /*$em->remove($product);*/
+
+        /*$product = new Product;
+        $product
+                ->setName('Table en metal')
+                ->setPrice(3000)
+                ->setSlug('table-en-metal');
+        $em->persist($product); // stock les données*/
+       /* $em->flush(); // Afficher les données
+
+        dd($product);*/
         return $this->render('home.html.twig');
     }
 
 }
 
+// name et un slug
+//string 255 not null
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class AppFixtures extends Fixture
+{
+    protected $sluggers;
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->sluggers = $slugger;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new \Liior\Faker\Prices($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
+
+
+        // CREATION DE FAUX PRODUIT DANS LA BASE DE DONNEES
+
+    /*for( $p = 0 ; $p < 100; $p++){
+        $product = new Product();
+        $product->setName($faker->sentence())
+            ->setPrice(mt_rand(100,200))
+            ->setSlug($faker->slug);
+        $manager->persist($product);
+    }*/
+
+            for($c = 0;$c < 3; $c++){
+                $category = new Category;
+                $category->setName($faker->department)
+                    ->setSlug(strtolower($this->sluggers->slug($category->getName())));
+
+                $manager->persist($category);
+                for( $p = 0 ; $p < mt_rand(15,20); $p++){
+                    $product = new Product();
+                    $product->setName($faker->productName)
+                        ->setPrice($faker->price(4000,20000))
+                        ->setSlug(strtolower($this->sluggers->slug($product->getName())))
+                        ->setCategory($category);
+                    $manager->persist($product);
+            }
+        }
+
+
+
+        $manager->flush();
+    }
+}
